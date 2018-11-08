82aae3019b0de4256a38608e14be8978c944f0ff
Muhammad Shahrom Ali <estineali@gmail.com>
2018-11-08 20:17:29 +0500

    Add files via upload

---
+++ HeaderFiles/BackgroundMotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Applied to:
+ Background Capsule
+ */
+
+
+public class BackgroundMotion : MonoBehaviour
+{
+
+    public float tumble;
+    public static bool flag = false;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        flag = false;
+    }
+    private void Update()
+    {
+        if (!flag)
+        {
+            rb.transform.Rotate(0f, tumble * Time.deltaTime, 0f);
+        }
+    }
+}
