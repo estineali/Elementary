89fb0655748322baa534fdce3fee7a554e10c71f
sarajameel <43106331+sarajameel@users.noreply.github.com>
2018-11-09 10:12:42 +0500

    Update Food.h

--- HeaderFiles/Food.h
+++ HeaderFiles/Food.h
@@ -1,15 +1,33 @@
 #pragma once 
 #include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+
+struct Position
+{
+	float x;
+	float y;
+	Position(float p_x, float p_y)
+	{
+		x = p_x;
+		y = p_y;
+	}
+};
 
 class Food
 {
 private:
 	int Health;
-	string Type_food;
-	\\ Sprite;
+	std::string Type_food;
+	//Sprite;
 	bool is_active;
 public:
 	Food();
+	void CollisionDetection();
+	void set_active(bool);
+	bool get_active();
+	Position get_position();
 	~Food();
 	
 	
