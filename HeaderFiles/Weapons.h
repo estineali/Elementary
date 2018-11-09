c6aac6bd002b60913e1eee0e8661549cd8e15981
sarajameel <43106331+sarajameel@users.noreply.github.com>
2018-11-09 11:25:21 +0500

    Update Weapons.h

--- HeaderFiles/Weapons.h
+++ HeaderFiles/Weapons.h
@@ -1,10 +1,34 @@
 #pragma once
 #include <stdio.h>
+#include <iostream>
+
+struct Position
+{
+	float x;
+	float y;
+	Position(float p_x, float p_y)
+	{
+		x = p_x;
+		y = p_y;
+	}
+};
 
 class Weapons
 {
+private:
+    Position pos;
+    bool is_active;
+    //sprite;
+    std::string type_weapon; //primary or special weapon 
+    std::string weapon_name;
+    int damage;
 public:
 	Weapons();
+	void CollisionDetection();
+	void set_active(bool);
+	bool get_active();
+	Position get_position();
+	void move();   //tentative
 	~Weapons();
 	
 };
