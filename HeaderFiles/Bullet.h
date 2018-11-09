8c059cf59205692ccff48a456533c6e7073fe259
sarajameel <43106331+sarajameel@users.noreply.github.com>
2018-11-09 10:04:20 +0500

    Update Bullet.h

--- HeaderFiles/Bullet.h
+++ HeaderFiles/Bullet.h
@@ -1,10 +1,31 @@
 #pragma once 
 #include <stdio.h>
 
+
+struct Position
+{
+	float x;
+	float y;
+	Position(float p_x, float p_y)
+	{
+		x = p_x;
+		y = p_y;
+	}
+};
+
 class Bullet
 {
+private:
+	bool is_active;
+	Position pos;
+	//sprite
 public:
 	Bullet();
 	~Bullet();
+	void CollisionDetection();
+	void set_active(bool);
+	bool get_active();
+	Position get_position();
+	void Move();
 	
 };
