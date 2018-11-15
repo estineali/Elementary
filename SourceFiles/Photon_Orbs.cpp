122fe1fe5464558738d9ca120ed7fb5c4a27c941
sarajameel <43106331+sarajameel@users.noreply.github.com>
2018-11-15 12:26:45 +0500

    Update Photon_Orbs.cpp

--- SourceFiles/Photon_Orbs.cpp
+++ SourceFiles/Photon_Orbs.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "../HeaderFiles/Photon_Orbs.h"
+#include "Obstacles.h"	
+#include <iostream>
+	
+	
+	
+	Photon_Orbs::Photon_Orbs(){
+		points=10;                                        //discuss 
+		pos.x=0;     //for now
+    pos.y=0;     //for now 
+		is_active= true;
+	}
+	Photon_Orbs::~Photon_Orbs(){
+	}
+	Position Photon_Orbs::get_pos(){
+		return pos;
+	}
+	void Photon_Orbs::set_position(Position p_pos){
+			this->pos= p_pos;
+	}
+	bool Photon_Orbs::get_active(){
+		return is_active;	
+	}
+	void Photon_Orbs::set_active(bool t_f){
+			this->is_active= is_active;
+	}
+	bool Photon_Orbs::collision(){                         //discuss
+		
+	}
+	void Photon_Orbs::levitate(){                          //discuss 
+		
+	}
