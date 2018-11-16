81c2a0e57f694936b8acdf5202f67d6d9271f8e8
Muhammad Shahrom Ali <estineali@gmail.com>
2018-11-16 11:41:36 +0500

    Add files via upload

---
+++ HeaderFiles/LinkedList.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+struct Node
+{
+	int m_data;
+	Node* m_node_ahead; //pointer that points to the item above
+	Node() 
+	{
+		m_node_ahead = NULL;
+	}
+};
+
+class LinkedList
+{
+
+private:
+	Node* m_first;
+	int m_size;
+	 
+public:
+	LinkedList();//Constructor
+	LinkedList(int val);//Overloaded Constructor
+	~LinkedList();//Destructor
+
+	int at(int indx);
+	//void set(int indx, int n);
+	void insert(int indx, int n);
+	void append(int n);
+	//void remove(int indx);
+
+	int size();
+	//int find(int n);
+	bool is_empty();
+	void show();
+};
